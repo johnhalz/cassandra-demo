be96ecae4be78b1a14a633bb9ec0770b5a82d4d8
John Halazonetis <john.halazonetis@icloud.com>
2023-05-22 11:15:05 +0200

    Added elements to test script

--- apps/connect.py
+++ apps/connect.py
@@ -1,8 +1,11 @@
 '''Cassandra interface'''
 import logging
+from datetime import datetime
+
+import pandas as pd
 
 from cassandra_interface import CassandraInterface
-from cassandra_interface.models import AU, HardwareType, Mirror, Hardware
+from cassandra_interface.models import AU, HardwareType, Mirror
 
 def setup_logger():
     '''Setup logger for app'''
@@ -14,10 +17,44 @@ def setup_logger():
 
 def main():
     # Connect to cassandra cluster
-    cas_session = CassandraInterface(host='0.0.0.0', port=9042, keyspace='au5000')
+    cas_session = CassandraInterface(host='0.0.0.0', port=9042)
 
-    # List keyspaces
-    cas_session.create_table(keyspace='au5000', table_model=AU)
+    # Create keyspace (if not already existing)
+    new_keyspace = 'au5000'
+    cas_session.create_keyspace(keyspace_name=new_keyspace)
+
+    # Create table in keyspace
+    cas_session.create_table(keyspace=new_keyspace, table_model=AU)
+
+    # Create element in new table
+    _ = AU.create(
+        serial_number='11-AU-3475-4538',
+        order_number=258234,
+        name='AU-M730',
+        hardware_type=HardwareType.AU.value,
+        mirror=Mirror.UNKNOWN.value,
+        variant='AU5k-V03',
+        created_on=datetime.now()
+    )
+
+    # Query oldest element in table
+    au_table = cas_session.table_to_df()
+    oldest_au_index = au_table['created_on'].idxmin()
+    oldest_au = au_table.loc[oldest_row_index]
+
+
+
+    # Delete element from table
+    AU(hardware_id=oldest_au.hardware_id, serial_number=oldest_au.serial_number, order_number=oldest_au.order_number).timeout(None).delete()
+    # cas_session.delete_row(
+    #     keyspace_name='au5000',
+    #     table_name='hardware',
+    #     primary_key_values={
+    #         'hardware_id':oldest_au.hardware_id,
+    #         'serial_number':oldest_au.serial_number,
+    #         'order_number':oldest_au.order_number
+    #     }
+    # )
 
     # Close connection to database
     cas_session.disconnect()
